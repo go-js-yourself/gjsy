aae4f054c79db52d41dc15402106b7cf19a5f16b
Miles Possing <7330275+milespossing@users.noreply.github.com>
2022-07-17 10:13:55 -0700

    First pass at tokenizing

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,15 +7,50 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+`
+	input := `
+	let a = 10;
+	let b = 20;
+	let c = a + b;
+
+	function test() {
+		return a + b + c;
+	}
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "a"},
 		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "b"},
+		{token.ASSIGN, "="},
+		{token.INT, "20"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "c"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "a"},
 		{token.PLUS, "+"},
-		{token.EOF, ""},
+		{token.IDENT, "b"},
+		{token.SEMICOLON, ";"},
+		{token.FUNCTION, "function"},
+		{token.IDENT, "test"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.IDENT, "a"},
+		{token.PLUS, "+"},
+		{token.IDENT, "b"},
+		{token.PLUS, "+"},
+		{token.IDENT, "c"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 	}
 
 	l := New(input)
--- token/token.go
+++ token/token.go
@@ -3,27 +3,44 @@ package token
 type TokenType string
 
 type Token struct {
-	Type     TokenType
-	Literal  string
+	Type    TokenType
+	Literal string
 }
 
 const (
-	ILLEGAL  = "ILLEGAL"
-	EOF      = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
 
 	// Operators
-	ASSIGN   = "="
-	PLUS     = "+"
+	ASSIGN = "="
+	PLUS   = "+"
 
 	// Delimiters
-	COMMA    = ","
-	SEMICOLON= ";"
-	LPAREN   = "("
-	RPAREN   = ")"
-	LBRACE   = "{"
-	RBRACE   = "}"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
 
 	// Keywords
-	FUNCTION = "FUNCTION"
-	LET      = "LET"
+	LET      = "let"
+	FUNCTION = "function"
+	RETURN   = "return"
 )
+
+var keywords = map[string]TokenType{
+	"function": FUNCTION,
+	"let":      LET,
+	"return":   RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
