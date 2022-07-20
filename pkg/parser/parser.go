3975293f3a32b95babd5752fb0704ea0a1663ab3
Ivan Valdes (@ivanvc) <iv@a.ki>
2022-07-20 09:23:03 -0700

    Add parsing errors

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,25 +1,42 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/go-js-yourself/gjsy/pkg/ast"
 	"github.com/go-js-yourself/gjsy/pkg/lexer"
 	"github.com/go-js-yourself/gjsy/pkg/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: make([]string, 0),
+	}
 
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	p.errors = append(
+		p.errors,
+		fmt.Sprintf("expected next token to be %s, got %s", t, p.peekToken.Type),
+	)
 }
 
 func (p *Parser) nextToken() {
@@ -84,7 +101,8 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
-	} else {
-		return false
 	}
+
+	p.peekError(t)
+	return false
 }
