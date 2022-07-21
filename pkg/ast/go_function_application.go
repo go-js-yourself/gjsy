d2a8669839bc5aa73295848c553d79dfd6862ed6
Ivan Valdes (@ivanvc) <iv@a.ki>
2022-07-21 11:17:27 -0700

    Prefix GFA String() with 'go'

--- pkg/ast/go_function_application.go
+++ pkg/ast/go_function_application.go
@@ -1,8 +1,16 @@
 package ast
 
-import "github.com/go-js-yourself/gjsy/pkg/token"
+import (
+	"fmt"
+
+	"github.com/go-js-yourself/gjsy/pkg/token"
+)
 
 type GoFunctionApplication struct {
 	Token token.Token
 	*FunctionApplication
+}
+
+func (gfa *GoFunctionApplication) String() string {
+	return fmt.Sprintf("go %s", gfa.FunctionApplication.String())
 }
