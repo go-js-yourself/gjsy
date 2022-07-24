cc1f11416fb643df20cc7a44bd153d7835e816c1
Miles Possing <7330275+milespossing@users.noreply.github.com>
2022-07-24 11:09:02 -0700

    Expanding tests

--- pkg/evaluator/let_test.go
+++ pkg/evaluator/let_test.go
@@ -8,6 +8,10 @@ func TestLetStatements(t *testing.T) {
 		expected int64
 	}{
 		{"let a = 5;a;", 5},
+		{"let a = 8 * 8; a;", 64},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c", 15},
+		{"let a = 5; a + 5", 10},
 	}
 
 	for _, tt := range tests {
