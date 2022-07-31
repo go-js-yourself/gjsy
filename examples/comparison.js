e7039e68a6ec094d59f3ff3bd42973bd09ec1672
Ivan Valdes (@ivanvc) <iv@a.ki>
2022-07-30 17:59:51 -0700

    Add more examples

---
+++ examples/comparison.js
@@ -0,0 +1,27 @@
+var a = true;
+var b = false;
+
+console.log("a =", a);
+console.log("b =", b);
+console.log("a && b =", a && b)
+console.log("a && a =", a && a)
+console.log("a || a =", a || a)
+console.log("a || b =", a || b)
+console.log("b || b =", b || b)
+console.log("!b || b =", !b || b)
+console.log("a == a", a == a)
+console.log("a == b", a == b)
+console.log("a != b", a != b)
+
+a = 10
+b = 20
+
+console.log("a =", a);
+console.log("b =", b);
+console.log("a >= b =", a >= b)
+console.log("a > b =", a > b)
+console.log("a <= b =", a <= b)
+console.log("a < b =", a < b)
+console.log("a == a =", a == a)
+console.log("a == b =", a == b)
+console.log("a != b =", a != b)
---
+++ examples/math.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 15;
+var c = 20;
+
+console.log("a =", a);
+console.log("b =", b);
+console.log("c =", c);
+console.log("a + b * c =", a + b * c);
+console.log("a * b + c =", a * b + c);
+console.log("a % c =", a % c);
+console.log("-a * b =", -a * c);
